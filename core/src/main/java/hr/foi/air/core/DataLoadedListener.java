35cc39849b0ed12cb4374798e470e9fadf5c8686
isvogor-foi <isvogor@foi.hr>
2016-10-10 09:37:20 +0200

    core abstract class and interface added

--- core/src/main/java/hr/foi/air/core/DataLoadedListener.java
+++ core/src/main/java/hr/foi/air/core/DataLoadedListener.java
@@ -1,8 +1,14 @@
 package hr.foi.air.core;
 
+import java.util.ArrayList;
+
+import hr.foi.air.database.entities.Discount;
+import hr.foi.air.database.entities.Store;
+
 /**
  * Created by Ivan on 10.10.2016..
  */
 
-public abstract class DataLoadedListener {
+public interface DataLoadedListener {
+    void onDataLoaded(ArrayList<Store> stores, ArrayList<Discount> discounts);
 }
--- core/src/main/java/hr/foi/air/core/DataLoader.java
+++ core/src/main/java/hr/foi/air/core/DataLoader.java
@@ -1,8 +1,32 @@
 package hr.foi.air.core;
 
+import java.util.ArrayList;
+
+import hr.foi.air.database.entities.Discount;
+import hr.foi.air.database.entities.Store;
+
 /**
  * Created by Ivan on 10.10.2016..
  */
 
-public class DataLoader {
+public abstract class DataLoader {
+
+    public ArrayList<Store> stores;
+    public ArrayList<Discount> discounts;
+
+    protected DataLoadedListener mDataLoadedListener;
+
+    public void loadData(DataLoadedListener dataLoadedListener){
+        this.mDataLoadedListener = dataLoadedListener;
+    }
+
+    public boolean dataLoaded(){
+        if(stores == null || discounts == null){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    
 }

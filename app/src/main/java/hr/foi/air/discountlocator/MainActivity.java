5ef25582a405e928fcdcd5d34d84a1c7217b43cf
zstapic <zlatko.stapic@foi.hr>
2016-11-08 10:37:17 +0100

    05 Replacing action bar with toolbar

--- app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
+++ app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -34,6 +35,9 @@ public class MainActivity extends AppCompatActivity  implements SharedPreference
 
         PreferenceManager.getDefaultSharedPreferences(this)
                 .registerOnSharedPreferenceChangeListener(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         DiscountListFragment mDiscountListFragment = new DiscountListFragment();
         FragmentManager mFragmentManager = getFragmentManager();

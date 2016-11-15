89630be18eb2fbc4f01770702ec3f5e390468ec6
zstapic <zlatko.stapic@foi.hr>
2016-11-15 06:27:08 +0100

    06 Adding empty fragment for map

---
+++ map/src/main/java/hr/foi/air/discountlocator/map/MapFragment.java
@@ -0,0 +1,17 @@
+package hr.foi.air.discountlocator.map;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MapFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.map_fragment, container, false);
+    }
+}

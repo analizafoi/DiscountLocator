55b6ce602b9a1e52b2af7a7489634d2a816a8be3
zstapic <zlatko.stapic@foi.hr>
2016-11-22 00:45:40 +0100

    07 Morphing map fragment to fit navigation item interface

--- map/src/main/java/hr/foi/air/discountlocator/map/MapFragment.java
+++ map/src/main/java/hr/foi/air/discountlocator/map/MapFragment.java
@@ -6,12 +6,37 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import hr.foi.air.core.NavigationItem;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements NavigationItem {
+
+    private int position;
+    private String name = "Map view";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.map_fragment, container, false);
     }
+
+    @Override
+    public String getItemName() {
+        return name;
+    }
+
+    @Override
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    @Override
+    public Fragment getFragment() {
+        return this;
+    }
+
 }

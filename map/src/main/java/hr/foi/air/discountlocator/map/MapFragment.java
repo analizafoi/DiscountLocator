73a3dfa8411141825678825ac84df4c52b334cd8
zstapic <zlatko.stapic@foi.hr>
2016-11-22 04:52:22 +0100

    07 Showing google map and getting references

--- map/src/main/java/hr/foi/air/discountlocator/map/MapFragment.java
+++ map/src/main/java/hr/foi/air/discountlocator/map/MapFragment.java
@@ -8,22 +8,45 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.util.ArrayList;
+
 import hr.foi.air.core.NavigationItem;
 import hr.foi.air.core.ReadyForDataListener;
 import hr.foi.air.database.entities.Discount;
 import hr.foi.air.database.entities.Store;
 
-public class MapFragment extends Fragment implements NavigationItem {
+public class MapFragment extends Fragment implements NavigationItem, OnMapReadyCallback {
 
     private int position;
     private String name = "Map view";
     private ReadyForDataListener readyForDataListener;
+    private com.google.android.gms.maps.MapFragment mapFragment;
+    private GoogleMap map = null;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.map_fragment, container, false);
+        View v = inflater.inflate(R.layout.map_fragment, container, false);
+        mapFragment = new com.google.android.gms.maps.MapFragment();
+        getFragmentManager().beginTransaction().add(R.id.frame, mapFragment).commit();
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
     }
 
     @Override

9857373554ccedb04abbb6019d7ff5a6f5025680
zstapic <zlatko.stapic@foi.hr>
2016-11-29 06:17:48 +0100

    08 Receiving FCM messages with default notificaitons (only if app is in background) According to: https://firebase.google.com/docs/notifications/android/console-device

---
+++ app/src/main/java/hr/foi/air/discountlocator/services/MyFirebaseMessagingService.java
@@ -0,0 +1,12 @@
+package hr.foi.air.discountlocator.services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Zlatko on 29.11.2016..
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+}

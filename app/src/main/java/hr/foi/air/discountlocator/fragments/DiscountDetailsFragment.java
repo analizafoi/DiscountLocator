775d352e3da5c2fe8d08409d11f3168b060d4957
zstapic <zlatko.stapic@foi.hr>
2016-11-29 10:53:36 +0100

    09 Show interstitial ads  - add code with logic to show interstitial ads where desired  - in our example, an interstitial ad is show every time the details on discount are shown

--- app/src/main/java/hr/foi/air/discountlocator/fragments/DiscountDetailsFragment.java
+++ app/src/main/java/hr/foi/air/discountlocator/fragments/DiscountDetailsFragment.java
@@ -8,12 +8,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
 import java.text.SimpleDateFormat;
 
 import hr.foi.air.database.entities.Discount;
 import hr.foi.air.discountlocator.R;
+import hr.foi.air.discountlocator.ads.DlAdsListener;
 
 public class DiscountDetailsFragment extends Fragment{
+    private InterstitialAd mInterstitial;
+    private static final String AD_UNIT_ID = "ca-app-pub-8639732656343372/9330745843";
 
     @Nullable
     @Override
@@ -44,5 +50,19 @@ public class DiscountDetailsFragment extends Fragment{
             txtStartDate.setText(sdf.format(discount.getStartDate()));
             txtEndDate.setText(" -- " + sdf.format(discount.getEndDate()));
         }
+
+        //interstitial ad
+        mInterstitial = new InterstitialAd(this.getActivity());
+        mInterstitial.setAdUnitId(AD_UNIT_ID);
+        mInterstitial.setAdListener(new DlAdsListener(this.getActivity()) {
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                mInterstitial.show();
+            }
+        });
+
+        mInterstitial.loadAd(new AdRequest.Builder().build());
+
     }
 }

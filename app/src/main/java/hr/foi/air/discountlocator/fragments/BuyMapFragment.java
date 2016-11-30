98d5b95a295d3cc5257758423f890557efafba0a
zstapic <zlatko.stapic@foi.hr>
2016-12-01 00:53:19 +0100

    09 Enable user to buy map  - change onViewCreated to add code to set click listener on buy button  - start purchase logic in new click listener on buy button (onBuyMapButtonClicked)
    Note: Remember that IabPerformer is set up to buy test product and not to verify purchase.
    In production version this SHOULD be changed.

--- app/src/main/java/hr/foi/air/discountlocator/fragments/BuyMapFragment.java
+++ app/src/main/java/hr/foi/air/discountlocator/fragments/BuyMapFragment.java
@@ -1,5 +1,6 @@
 package hr.foi.air.discountlocator.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -15,14 +17,32 @@ import hr.foi.air.discountlocator.R;
 import hr.foi.air.core.NavigationItem;
 import hr.foi.air.database.entities.Discount;
 import hr.foi.air.database.entities.Store;
+import hr.foi.air.discountlocator.iab.IabPerformer;
 
 public class BuyMapFragment extends Fragment implements NavigationItem {
     int position = 0;
+    Activity mActivity;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.buy_map_fragment, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mActivity = this.getActivity();
+
+        Button btnMapBuy = (Button) mActivity.findViewById(R.id.btn_map_buy);
+        btnMapBuy.setOnClickListener(onBuyMapButtonClicked);
+    }
+
+    public View.OnClickListener onBuyMapButtonClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            IabPerformer.BuyMap(mActivity);
+        }
+    };
 
     @Override
     public String getItemName() {
